f34d80e66bf6dd0835b410b63b5abcea2e65bde4
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-07-09 15:15:14 +0200

    m

--- src/utils/paretaClient.js
+++ src/utils/paretaClient.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-if (process.env.NODE_ENV === "development") {
-	axios.defaults.baseURL = process.env.REACT_APP_BACKEND_API_LOCAL;
-} else {
-	axios.defaults.bseURL = process.env.REACT_APP_BACKEND_API;
-}
+// if (process.env.NODE_ENV === "development") {
+// 	axios.defaults.baseURL = process.env.REACT_APP_BACKEND_API_LOCAL;
+// } else {
+axios.defaults.bseURL = process.env.REACT_APP_BACKEND_API;
+// }
 
 const refresh = () => {
 	const token = Cookies.get("parent-token");
