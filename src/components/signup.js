953466910a1e5360769feb06e4806c2e7668e9d4
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-07-09 14:53:01 +0200

    m

--- src/components/signup.js
+++ src/components/signup.js
@@ -158,20 +158,20 @@ const SignUp = ({ handleSignUp, handleSignUpSubmit }) => {
 	// 		progress: undefined,
 	// 	});
 
-	// useEffect(() => {
-	// 	const notify = () =>
-	// 		toast.error(`🙊 ${errorMessage}`, {
-	// 			position: "top-center",
-	// 			autoClose: 5000,
-	// 			hideProgressBar: false,
-	// 			closeOnClick: true,
-	// 			pauseOnHover: true,
-	// 			draggable: true,
-	// 			progress: undefined,
-	// 		});
-	// 	if (errorMessage) notify();
-	// 	setErrorMessage("");
-	// }, [setErrorMessage, errorMessage]);
+	useEffect(() => {
+		// const notify = () =>
+		// 	toast.error(`🙊 ${errorMessage}`, {
+		// 		position: "top-center",
+		// 		autoClose: 5000,
+		// 		hideProgressBar: false,
+		// 		closeOnClick: true,
+		// 		pauseOnHover: true,
+		// 		draggable: true,
+		// 		progress: undefined,
+		// 	});
+		// if (errorMessage) notify();
+		setErrorMessage("");
+	}, [setErrorMessage, errorMessage]);
 
 	return (
 		<Fragment>
